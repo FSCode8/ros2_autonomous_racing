3827200d933e1c26e5a9cb891f0ee4e3e40c3969
Alexander Schwoerer <alexander@dr-schwoerer.de>
2025-04-24 11:07:20 +0000

    Fixed Syntax and Bugs

--- car_controller/include/car_controller/ttc.h
+++ car_controller/include/car_controller/ttc.h
@@ -10,19 +10,19 @@ class TTC_Node : public rclcpp::Node
 {
   public:
     //constructor
-    TTC_Node(){};
+    TTC_Node();
 
   private:
     //variables
     rclcpp::TimerBase::SharedPtr timer;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr ttc_publisher;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr dist_subscription;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr vel_subscription;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr ttc_publisher;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr dist_subscription;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr vel_subscription;
     size_t count;
     float ttc, distance, velocity;
 
     //methods
-    void ttc_callback(){};
-    void distance_callback(const std_msgs::msg::String & msg) const{};
-    void velocity_callback(const std_msgs::msg::String & msg) const{};
+    void ttc_callback();
+    void distance_callback(const std_msgs::msg::Float32 & msg);
+    void velocity_callback(const std_msgs::msg::Float32 & msg);
 };
--- car_controller/src/ttc.cpp
+++ car_controller/src/ttc.cpp
@@ -6,22 +6,24 @@ using namespace std::chrono_literals;
 * member function as a callback from the timer. */
 
 TTC_Node::TTC_Node() : Node("ttc_node"), count(0){
-  ttc_publisher = this->create_publisher<std_msgs::msg::float32>("time_to_collision", 10);
-  dist_subscription = this->create_subscription<std_msgs::msg::float32>("distance_to_object", 10, std::bind(&TTC_Node::distance_callback, this, _1));
-  dist_subscription = this->create_subscription<std_msgs::msg::float32>("", 10, std::bind(&TTC_Node::velocity_callback, this, _1)); //Add topic which sends twist msgs
-  timer_ = this->create_wall_timer(10ms, std::bind(&TTC_Node::ttc_callback, this));
+  ttc_publisher = this->create_publisher<std_msgs::msg::Float32>("time_to_collision", 10);
+  dist_subscription = this->create_subscription<std_msgs::msg::Float32>("distance_to_object", 10, std::bind(&TTC_Node::distance_callback, this, std::placeholders::_1));
+  vel_subscription = this->create_subscription<std_msgs::msg::Float32>("", 10, std::bind(&TTC_Node::velocity_callback, this, std::placeholders::_1)); //Add topic which sends twist msgs
+  timer = this->create_wall_timer(10ms, std::bind(&TTC_Node::ttc_callback, this));
 }
 
 void TTC_Node::ttc_callback(){
+  std_msgs::msg::Float32 ttc_msg;
+  ttc_msg.data = ttc;
   ttc = distance / velocity;
-  publisher_->publish(ttc);
+  ttc_publisher->publish(ttc_msg);
 }
 
-void TTC_Node::distance_callback(const std_msgs::msg::String & msg) const{
+void TTC_Node::distance_callback(const std_msgs::msg::Float32 & msg){
   distance = msg.data;
 }
 
-void TTC_Node::velocity_callback(const std_msgs::msg::String & msg) const{
+void TTC_Node::velocity_callback(const std_msgs::msg::Float32 & msg){
   velocity = msg.data;  // Figure out which parameter in message holds velocity
 }
 
