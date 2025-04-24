d9c0db79401465b66064e963d90a72ba291e296d
Alexander Schwoerer <alexander@dr-schwoerer.de>
2025-04-24 13:10:46 +0000

    small stuff

--- car_controller/src/ttc.cpp
+++ car_controller/src/ttc.cpp
@@ -2,14 +2,11 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
 TTC_Node::TTC_Node() : Node("ttc_node"), count(0){
   ttc_publisher = this->create_publisher<std_msgs::msg::Float32>("time_to_collision", 10);
-  dist_subscription = this->create_subscription<std_msgs::msg::Float32>("distance_to_object", 10, std::bind(&TTC_Node::distance_callback, this, std::placeholders::_1));
+  dist_subscription = this->create_subscription<std_msgs::msg::Float32>("collision_distance", 10, std::bind(&TTC_Node::distance_callback, this, std::placeholders::_1));
   vel_subscription = this->create_subscription<std_msgs::msg::Float32>("", 10, std::bind(&TTC_Node::velocity_callback, this, std::placeholders::_1)); //Add topic which sends twist msgs
-  timer = this->create_wall_timer(10ms, std::bind(&TTC_Node::ttc_callback, this));
+  timer = this->create_wall_timer(33ms, std::bind(&TTC_Node::ttc_callback, this));
 }
 
 void TTC_Node::ttc_callback(){
